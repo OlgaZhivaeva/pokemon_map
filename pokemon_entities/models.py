d59d18a57ee6c9e15e92c5bea8a868d2446ed568
OlgaZhivaeva <olga.zhiv@yandex.ru>
2024-06-23 22:13:59 +0300

    Add pokemon description, previous_evolution

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,6 +4,10 @@ from django.db import models  # noqa F401
 class Pokemon(models.Model):
     title = models.CharField(max_length=200, blank=True)
     image = models.ImageField(null=True)
+    title_en = models.CharField(max_length=200, blank=True)
+    title_jp = models.CharField(max_length=200, blank=True)
+    description = models.TextField(max_length=1000, blank=True)
+    previous_evolution = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.title
--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -66,7 +66,15 @@ def show_pokemon(request, pokemon_id):
         'pokemon_id': pokemon.id,
         'img_url': request.build_absolute_uri(pokemon.image.url),
         'title': pokemon.title,
-        'title_ru': pokemon.title
+        'title_ru': pokemon.title,
+        'title_en': pokemon.title_en,
+        "title_jp": pokemon.title_jp,
+        'description': pokemon.description,
+        'previous_evolution': {
+            'pokemon_id': pokemon.previous_evolution.id,
+            'title_ru': pokemon.previous_evolution.title,
+            'img_url': request.build_absolute_uri(pokemon.previous_evolution.image.url)
+        }
     }
 
 
